ec4c5136d82f46f435b8cafa3683b68f48b44447
Tiago Morais <rpm.tiago@gmail.com>
2020-04-18 17:25:25 +0100

    feat(data): Change `about` page from inpage content to cms
    Fetch `about.tsx` page content from content cms

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,23 +1,31 @@
 import { Layout } from "../components/layout/Layout";
+import { GetStaticProps } from "next";
+import { fetchEntryBySlug } from "../services/contentService";
+import { RichText } from "../components/content/RichText/RichText";
 
-const AboutPage = () => (
+interface IAboutContent {
+  title: string;
+  content: string;
+}
+type IAboutPageProps = IAboutContent;
+
+const AboutPage = ({ content, title }) => (
   <Layout space={0}>
-    <Layout.Title>About me</Layout.Title>
+    <Layout.Title>{title}</Layout.Title>
     <Layout.Main>
-      <p>My name is Tiago Morais.</p>
-      <p>
-        I'm 27, born in sunny Lisbon and I've been living and working in busy London for the past 5 years. I've been playing around with computer
-        since I was little and have been writing software professionally since 2010.
-      </p>
-      <p>
-        I've worked with .net and specially Javascript my entire career. I have experience leading teams, hiring and planning, but my main focus is
-        technical excellence.
-        <br />
-        Over the past few years I've specialliased in the web, but I have a special interest in Scalability, User Experience and Design Systems.
-      </p>
+      <RichText content={content} />
     </Layout.Main>
     <Layout.LinkBack to="/" />
   </Layout>
 );
+
+export const getStaticProps: GetStaticProps<IAboutPageProps> = async ({ preview }) => {
+  const content = await fetchEntryBySlug<IAboutContent>(preview, "staticPage", "about");
+  return {
+    props: {
+      ...content?.data,
+    },
+  };
+};
 
 export default AboutPage;
