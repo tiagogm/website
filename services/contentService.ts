dfaa38071c22bf07d604f94f0d249b00eb751875
Tiago Morais <rpm.tiago@gmail.com>
2020-04-19 16:15:21 +0100

    feat(services): :scissors:  Improve `contentService` to flatten resolved links objects
    Since link resolution object format does not work the same way in the REST API
    as it does in contenful-js lib, a lot of wrangling might need to be required
    to resolve links and abtrasct contenful data shapes from components.
    Which means it's prob better to stick with contenful-js instead of vanila
    REST calls.

--- services/contentService.ts
+++ services/contentService.ts
@@ -3,16 +3,36 @@ import config from "../config";
 import urljoin from "urljoin";
 import { logService } from "./logService";
 
+/**
+ * @todo Use contenful.js client to simplify all this
+ */
+
 //
 // Types
 //
 
+interface IContentInfo {
+  type: "Link";
+  linkType: "Asset" | "Entry";
+  id: string;
+}
+
+interface IContentItem<T> {
+  fields: T & {
+    sys: IContentInfo;
+  };
+  sys: IContentInfo;
+}
+
+interface IContentLinks<T> {
+  Entry?: IContentItem<T>[];
+  Asset?: IContentItem<T>[];
+}
+
 //use contentful types (todo: import from somewhere)
 interface IContentEntry<T> {
-  items: {
-    fields: T;
-    //others
-  }[];
+  items: IContentItem<T>[];
+  includes?: IContentLinks<T>;
   limit: number;
   skip: number;
   total: number;
@@ -22,11 +42,12 @@ interface IContentEntry<T> {
 interface IContentResponse<T> {
   success: boolean;
   data?: T;
+  links?: IContentLinks<any>;
   error?: Error;
 }
 
 //
-// Reusable func
+// Util func
 //
 
 //TODO: Improve this bit, abstract from vendor
@@ -34,8 +55,10 @@ const getEnvDomain = (preview: boolean) => (preview ? config.CMS_PREVIEW_DOMAIN 
 const getEnvToken = (preview: boolean) => (preview ? config.CMS_PREVIEW_API_TOKEN : config.CMS_API_TOKEN);
 const getBaseUrl = (preview: boolean) => urljoin(getEnvDomain(preview), "spaces", config.CMS_SPACEID_SITE, "environments", config.CMS_ENV);
 
-const createSuccessResponse = <T>(data?: T): IContentResponse<T> => ({
+//
+const createSuccessResponse = <T>(data?: T, links?: IContentLinks<any>): IContentResponse<T> => ({
   data,
+  links,
   success: true,
 });
 
@@ -50,6 +73,54 @@ const fetchWithAuth = async (url: string, preview?: boolean) =>
       Authorization: `Bearer ${getEnvToken(preview)}`,
     },
   });
+
+//
+// Data wrangling
+//
+
+/**
+ * Find links in "includes" and merged those link.fields into the origignal field
+ * This flattens linked items together to simplify components
+ */
+const assignLinkToField = (field: IContentItem<any>, includes: IContentLinks<any>) => {
+  if (!field.sys || field.sys.type !== "Link") {
+    return field;
+  }
+
+  const linktype = field.sys.linkType;
+  if (!includes[linktype]) {
+    return field;
+  }
+
+  var link = includes[linktype].find((a) => a.sys.id === field.sys.id);
+  if (!link) {
+    return field;
+  }
+
+  return Object.assign(field, link.fields);
+};
+
+/**
+ * Map over all items fields and merge their linked fields together
+ */
+const assignLinkToFieldItems = (itemsFields, includes: IContentLinks<any>) => {
+  itemsFields.map((fields) => {
+    for (const k in fields) {
+      var field = fields[k];
+
+      if (field instanceof Array) {
+        field = field.map((dfk) => assignLinkToField(dfk, includes));
+      } else {
+        field = assignLinkToField(field, includes);
+      }
+    }
+  });
+  return itemsFields;
+};
+
+//
+// Methods
+//
 
 /**
  * Fetch entries from content source (contentful)
@@ -85,11 +156,17 @@ const fetchEntryBySlug = async <T>(preview: boolean, contentType: string, slug: 
     const response = await fetchWithAuth(url, preview);
     const responseJson = (await response.json()) as IContentEntry<T>;
 
-    const items = (responseJson.items || []).map((i) => i.fields);
-    if (items.length <= 0) {
-      return createFailedResponse(new Error(`contentService.fetchEntryBySlug - No results returned for slug:${slug}`));
+    const itemsFields = (responseJson.items || []).map((i) => i.fields);
+    const includes = responseJson.includes;
+
+    if (itemsFields.length !== 1) {
+      return createFailedResponse(new Error(`contentService.fetchEntryBySlug - results for slug:${slug} either empty or > 1`));
     }
-    return createSuccessResponse(items[0]);
+
+    //assign linked fields
+    const itemFields = assignLinkToFieldItems(itemsFields, responseJson.includes)[0];
+
+    return createSuccessResponse(itemFields, includes);
   } catch (e) {
     logService.exception(e);
     return createFailedResponse(e);
